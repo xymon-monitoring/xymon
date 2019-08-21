81656c6aa1fc0f5b466809ff6e0f2d113f68b4c4
jccleaver <jccleaver>
2019-08-21 15:38:53 +0000

    Don't crash on a missing allevents file

--- xymond/xymond_history.c
+++ xymond/xymond_history.c
@@ -170,8 +170,11 @@ int main(int argc, char *argv[])
 		alleventsfd = fopen(alleventsfn, "a");
 		if (alleventsfd == NULL) {
 			errprintf("Cannot open the all-events file '%s'\n", alleventsfn);
+			return 1;
 		}
-		setvbuf(alleventsfd, (char *)NULL, _IOLBF, 0);
+		else {
+			setvbuf(alleventsfd, (char *)NULL, _IOLBF, 0);
+		}
 	}
 
 	/* For picking up lost children */
