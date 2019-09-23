3366cfc480fe9fef88729adf3a5eb6ef28a3626b
jccleaver <jccleaver>
2019-09-23 23:24:19 +0000

    Add RRDADDUPDATED variable to control timestamping of RRD graphs (Thanks, John Horne)

--- web/showgraph.c
+++ web/showgraph.c
@@ -821,6 +821,8 @@ void generate_graph(char *gdeffn, char *rrddir, char *graphfn)
 	char **useropts = NULL;
 	int useroptcount = 0, useroptidx;
 
+	int addrrdupdated = (strcmp(xgetenv("RRDADDUPDATED"), "TRUE") == 0);
+
 	/* Find the graphs.cfg file and load it */
 	if (gdeffn == NULL) {
 		char fnam[PATH_MAX];
@@ -1178,12 +1180,14 @@ void generate_graph(char *gdeffn, char *rrddir, char *graphfn)
 		}
 	}
 
+	if (addrrdupdated) {
 #ifdef RRDTOOL12
-	strftime(timestamp, sizeof(timestamp), "COMMENT:Updated\\: %d-%b-%Y %H\\:%M\\:%S", localtime(&now));
+		strftime(timestamp, sizeof(timestamp), "COMMENT:Updated\\: %d-%b-%Y %H\\:%M\\:%S", localtime(&now));
 #else
-	strftime(timestamp, sizeof(timestamp), "COMMENT:Updated: %d-%b-%Y %H:%M:%S", localtime(&now));
+		strftime(timestamp, sizeof(timestamp), "COMMENT:Updated: %d-%b-%Y %H:%M:%S", localtime(&now));
 #endif
-	rrdargs[argi++] = strdup(timestamp);
+		rrdargs[argi++] = strdup(timestamp);
+	}
 
 
 	rrdargcount = argi; rrdargs[argi++] = NULL;
