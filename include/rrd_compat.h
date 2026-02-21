3e77c748b92a9c9cd8797755c749ab35c316f9e6
Bonomani <8841264+bonomani@users.noreply.github.com>
2026-02-21 07:48:37 +0100

    rrd: neutralize RRDTOOL12 and require explicit ABI gate

--- include/rrd_compat.h
+++ include/rrd_compat.h
@@ -20,42 +20,53 @@
 /*
  * Ad-hoc/manual builds that skip configure must opt in explicitly.
  */
-#if defined(RRDTOOL12)
-#define RRD_CONST_ARGS 0
+#if defined(RRDTOOL19)
+#define RRD_CONST_ARGS 1
 #elif defined(XYMON_ASSUME_RRD_MUTABLE_ARGS)
 #define RRD_CONST_ARGS 0
-#else
-#error "RRD_CONST_ARGS is not defined. Run configure or define XYMON_ASSUME_RRD_MUTABLE_ARGS=1."
+#elif defined(RRDTOOL12)
+/* RRDTOOL12 is obsolete and intentionally ignored. */
+#endif
+#ifndef RRD_CONST_ARGS
+#error "RRD_CONST_ARGS is not defined. Run configure or define RRDTOOL19 (or RRD_CONST_ARGS directly)."
 #endif
 #endif
 
 #if RRD_CONST_ARGS
 typedef const char *xymon_rrd_argv_item_t;
+static inline const char **xymon_rrd_api_argv(xymon_rrd_argv_item_t *argv)
+{
+	return (const char **)argv;
+}
 #else
 typedef char *xymon_rrd_argv_item_t;
+static inline char **xymon_rrd_api_argv(xymon_rrd_argv_item_t *argv)
+{
+	return (char **)argv;
+}
 #endif
 
 static inline int xymon_rrd_update(int argc, xymon_rrd_argv_item_t *argv)
 {
-	return rrd_update(argc, argv);
+	return rrd_update(argc, (void *)xymon_rrd_api_argv(argv));
 }
 
 static inline int xymon_rrd_create(int argc, xymon_rrd_argv_item_t *argv)
 {
-	return rrd_create(argc, argv);
+	return rrd_create(argc, (void *)xymon_rrd_api_argv(argv));
 }
 
 static inline int xymon_rrd_fetch(int argc, xymon_rrd_argv_item_t *argv,
 				  time_t *start, time_t *end, unsigned long *step, unsigned long *dscount,
 				  char ***dsnames, rrd_value_t **data)
 {
-	return rrd_fetch(argc, argv, start, end, step, dscount, dsnames, data);
+	return rrd_fetch(argc, (void *)xymon_rrd_api_argv(argv), start, end, step, dscount, dsnames, data);
 }
 
 static inline int xymon_rrd_graph(int argc, xymon_rrd_argv_item_t *argv, 
 				  char ***calcpr, int *xsize, int *ysize, double *ymin, double *ymax)
 { 
-	return rrd_graph(argc, argv, calcpr, xsize, ysize, NULL, ymin, ymax);
+	return rrd_graph(argc, (void *)xymon_rrd_api_argv(argv), calcpr, xsize, ysize, NULL, ymin, ymax);
 }
 
 #endif /* XYMON_RRD_COMPAT_H */
