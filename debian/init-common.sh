0c9a3a0ce98c7512306b28caf6ea2609117367ae
Roland Rosenfeld <roland@spinnaker.de>
2026-08-21 17:36:18 +0200

    debian/: More adaptions to 4.3.31 version.

--- debian/init-common.sh
+++ debian/init-common.sh
@@ -23,27 +23,5 @@ create_includefiles ()
 		echo "XYMSERVERS=\"$XYMONSERVERS\""
 	fi > /var/run/xymon/bbdisp-runtime.cfg
 
-	for cfg in /etc/xymon/clientlaunch.d/*.cfg ; do
-		test -e $cfg && echo "include $cfg"
-	done > /var/run/xymon/clientlaunch-include.cfg
-
-	for cfg in /etc/xymon/xymonclient.d/*.cfg ; do
-		test -e $cfg && echo "include $cfg"
-	done > /var/run/xymon/xymonclient-include.cfg
-
-	if test -x /usr/lib/xymon/server/bin/xymond ; then
-		for cfg in /etc/xymon/tasks.d/*.cfg ; do
-			test -e $cfg && echo "include $cfg"
-		done > /var/run/xymon/tasks-include.cfg
-
-		for cfg in /etc/xymon/graphs.d/*.cfg ; do
-			test -e $cfg && echo "include $cfg"
-		done > /var/run/xymon/graphs-include.cfg
-
-		for cfg in /etc/xymon/xymonserver.d/*.cfg ; do
-			test -e $cfg && echo "include $cfg"
-		done > /var/run/xymon/xymonserver-include.cfg
-	fi
-
 	return 0
 }
